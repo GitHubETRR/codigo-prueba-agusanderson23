e8c7980ac096d81b5988846e40cbcb92a21f1e75
agusanderson23 <aanderson@etrr.edu.ar>
2024-11-26 20:10:27 -0300

    practicas vectores

--- LE5/ejerciciovectores1.c
+++ LE5/ejerciciovectores1.c
@@ -1,55 +1,67 @@
 #include <stdio.h>
 #define CALIFICACIONMAX 10
 #define CALIFICACIONAPROBADA 7
-int PedirAlumnos();
-void CalcularYMostrar(int CantidadAlumnos);
-int PedirCalificaciones(int CantidadAlumnos, int Calificaciones[]);
-void ContarAlumnosAprobados(int Calificaciones[], int CantidadAlumnos);
+#define CALIFICACIONMIN 0
 
-int main(){
+int PedirAlumnos();
+float PedirCalificaciones(int CantidadAlumnos, float Calificaciones[]);
+void CalcularYMostrar(float Calificaciones[], int CantidadAlumnos, float SumaCalificaciones);
+void ContarAlumnosAprobados(float Calificaciones[], int CantidadAlumnos);
+
+int main() {
     int CantidadAlumnos = PedirAlumnos();
     float Calificaciones[CantidadAlumnos];
+    float SumaCalificaciones = PedirCalificaciones(CantidadAlumnos, Calificaciones);
+    CalcularYMostrar(Calificaciones, CantidadAlumnos, SumaCalificaciones);
+    ContarAlumnosAprobados(Calificaciones, CantidadAlumnos);
+
+    return 0;
 }
-int PedirAlumnos(){
+
+int PedirAlumnos() {
     int CantidadAlumnos;
-    printf("Ingrese la cantidad de alumnos\n");
+    printf("Ingrese la cantidad de alumnos:\n");
     scanf("%d", &CantidadAlumnos);
     return CantidadAlumnos;
 }
-void Calcular_Y_Mostrar(float Calificaciones[], int CantidadAlumnos, float SumaCalificaciones){
-    float CalificacionAlta;
+
+float PedirCalificaciones(int CantidadAlumnos, float Calificaciones[]) {
+    float SumaCalificaciones = 0;
+    for (int i = 0; i < CantidadAlumnos; i++) {
+        printf("Ingrese la calificación del alumno %d:\n", i + 1);
+        scanf("%f", &Calificaciones[i]);
+        SumaCalificaciones += Calificaciones[i];
+    }
+    return SumaCalificaciones;
+}
+
+void CalcularYMostrar(float Calificaciones[], int CantidadAlumnos, float SumaCalificaciones) {
+    float CalificacionAlta = CALIFICACIONMIN;
     float CalificacionBaja = CALIFICACIONMAX;
     float Promedio;
-    for(int i = 0; i < CantidadAlumnos; i++){
-        if (Calificaciones[i] > CalificacionAlta){
+
+    for (int i = 0; i < CantidadAlumnos; i++) {
+        if (Calificaciones[i] > CalificacionAlta) {
             CalificacionAlta = Calificaciones[i];
         }
-        
-    }
-    for(int i = 0; i < CantidadAlumnos; i++){
-        if (Calificaciones[i] < CalificacionBaja){
+        if (Calificaciones[i] < CalificacionBaja) {
             CalificacionBaja = Calificaciones[i];
         }
-    }
-    Promedio = (float) SumaCalificaciones / CantidadAlumnos;
-    printf("La calificacion mas baja fue %f", CalificacionBaja);
-    printf("La calificacion mas alta fue %f", CalificacionAlta);
-    printf("El promedio de calificaciones fue %f", Promedio);
+        printf("%f  ", Calificaciones[i]);
+    }printf("\n");
+    Promedio = SumaCalificaciones / CantidadAlumnos;
+
+    printf("\nLa calificación más baja fue: %f\n", CalificacionBaja);
+    printf("La calificación más alta fue: %f\n", CalificacionAlta);
+    printf("El promedio de calificaciones fue: %f\n", Promedio);
 }
-int PedirCalificaciones(int CantidadAlumnos, float Calificaciones[]){
-    float SumaCalificaciones;
-    for(int i = 0; i < CantidadAlumnos; i++){
-        printf("Ingrese las calificaciones de los alumnos\n");
-        scanf("%f", SumaCalificaciones);
-        return SumaCalificaciones;
-    }
-}
-void ContarAlumnosAprobados(int Calificaciones[], CantidadAlumnos){
-    int AlumnosAprobados=0;
-    for(int i = 0; i < CantidadAlumnos ; i++){
-        if(Calificaciones[i] => CALIFICACIONAPROBADA){
+
+void ContarAlumnosAprobados(float Calificaciones[], int CantidadAlumnos) {
+    int AlumnosAprobados = 0;
+    for (int i = 0; i < CantidadAlumnos; i++) {
+        if (Calificaciones[i] >= CALIFICACIONAPROBADA) {
             AlumnosAprobados++;
         }
     }
-    printf("Alumnos aprobados: %d\n", AlumnosAprobados);
+    printf("Cantidad de alumnos aprobados: %d\n", AlumnosAprobados);
 }
